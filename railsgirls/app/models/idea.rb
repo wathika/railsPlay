a8d78938c24c8576a6cf8159a98a688280a6e3a5
wathika <vcwathika@gmail.com>
2016-01-11 10:43:47 +0300

    deleted railsGirls

--- railsgirls/app/models/idea.rb
+++
@@ -1,5 +0,0 @@
-class Idea < ActiveRecord::Base
-
-  mount_uploader :picture, PictureUploader
-
-end
--- railsgirls/app/views/ideas/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @idea, :id, :name, :desription, :picture, :created_at, :updated_at
